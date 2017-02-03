e685f1d74b202a04e03fa5298e87271b9c35aee5
artem <lineartem@gmail.com>
2017-02-03 20:03:15 +0200

    skeleton for React App

--- Gruntfile.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Artem on 1/12/2017.
- */
-/*global module*/
-module.exports = function (grunt) {
-    "use strict";
-    require('load-grunt-tasks')(grunt);
-};
---
+++ test/components/About-test.js
@@ -0,0 +1,17 @@
+import test from 'ava';
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import About from '../../src/components/About';
+
+const shallowRenderer = TestUtils.createRenderer();
+shallowRenderer.render(<About />);
+const about = shallowRenderer.getRenderOutput();
+
+test('should have a div as container', t => {
+  t.is(about.type, 'div');
+});
+
+test('should contains an H2', t => {
+  t.is(about.props.children[0].type, 'h2');
+});
+
