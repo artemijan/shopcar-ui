4870564bfbd90068b7a80d5fb44780306b3d1c47
arrtem <artem.fedorov@logicify.com>
2017-03-18 15:14:10 +0200

    share store data between the components

--- app/js/components/MenuBar.js
+++ app/js/components/MenuBar.js
@@ -6,15 +6,33 @@ import AppBar from 'material-ui/AppBar';
 import { Link } from 'react-router';
 import FlatButton from 'material-ui/FlatButton';
 import  AuthControl from './AuthControl';
+import { connect } from 'react-redux';
+
+@connect(state => ({
+  SecurityContext: state.auth.SecurityContext,
+  SecurityPrincipal: state.auth.SecurityPrincipal
+}))
+
 class MenuItems extends Component {
+  constructor(props) {
+    super(props);
+    this.isLoggedIn = this.isLoggedIn.bind(this);
+  }
+
+  isLoggedIn() {
+    return this.props.SecurityContext.isLoggedIn;
+  }
 
   render() {
+    let authContent = <span>
+      <FlatButton containerElement={<Link to='/' />} label="Home" />
+        <FlatButton containerElement={<Link to='/about' />} label="About" />
+        <FlatButton containerElement={<Link to='/contacts' />} label="Contacts" />
+    </span>;
     return (
       <div className="menu-items-container">
         <AuthControl />
-        <FlatButton containerElement={<Link to='/' />} label="Home" />
-        <FlatButton containerElement={<Link to='/about' />} label="About" />
-        <FlatButton containerElement={<Link to='/contacts' />} label="Contacts" />
+        {this.isLoggedIn() ? authContent : ""}
       </div>
     );
   }
